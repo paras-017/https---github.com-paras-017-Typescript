a7292070d13e90ea734ba1fb45de7a4348db4a67
paras-017 <paraschauhan0017@gmail.com>
2023-06-10 17:56:07 +0530

    union

---
+++ 02_basics_union/myUnion.js
@@ -0,0 +1,3 @@
+var data1 = [1, 2, 3, 4, 5, 6, 7, 8];
+var data2 = ["one", "two", "three", "four", "five", "six"];
+var data = [1, 2, 3, 4, "hello", 6, '7', 8];
---
+++ 02_basics_union/myUnion.ts
@@ -0,0 +1,4 @@
+const data1: number[] = [1, 2, 3, 4, 5, 6, 7, 8]
+const data2: string[] = ["one", "two", "three", "four", "five", "six"]
+const data: (number|string)[] = [1, 2, 3, 4, "hello", 6, '7', 8]
+
